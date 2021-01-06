ff6310b81ebfe27e924f54b79aad2a3cac00e560
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2021-01-06 03:20:41 -0300

    POST /orders and PUT /orders/{id}/delivered

--- backend/deliver/src/main/java/com/devsuperior/deliver/controllers/OrderController.java
+++ backend/deliver/src/main/java/com/devsuperior/deliver/controllers/OrderController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,17 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = this.service.findAll();
 		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = this.service.insert(dto);
+		return new ResponseEntity<>(dto, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		OrderDTO dto = this.service.setDelivered(id);
+		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 }
--- backend/deliver/src/main/java/com/devsuperior/deliver/services/OrderService.java
+++ backend/deliver/src/main/java/com/devsuperior/deliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.deliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.deliver.dto.OrderDTO;
+import com.devsuperior.deliver.dto.ProductDTO;
 import com.devsuperior.deliver.entities.Order;
+import com.devsuperior.deliver.entities.OrderStatus;
+import com.devsuperior.deliver.entities.Product;
 import com.devsuperior.deliver.repositories.OrderRepository;
+import com.devsuperior.deliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,34 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
-	@Transactional(readOnly = true) //optional annotation to become speed code
+	@Autowired
+	private ProductRepository productRepository;
+	
+	//optional annotation to become speed code
+	@Transactional(readOnly = true) 
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			//getOne build product instance without go to database
+			Product product = this.productRepository.getOne(p.getId()); 
+			order.getProducts().add(product);
+		}
+		order = this.repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = this.repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = this.repository.save(order);
+		return new OrderDTO(order);
+	}
 }
